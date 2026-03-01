357c87d2fa569d888d70af06aa3b2324c7bd1c35
Jeri-Sta <jerielstarosky@hotmail.com>
2026-03-01 12:47:01 -0300

    #1 - Adicionado estrutura inicial do projeto

---
+++ src/main/java/br/com/starosky/entrepreneur/EntrepreneurApplication.java
@@ -0,0 +1,13 @@
+package br.com.starosky.entrepreneur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrepreneurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrepreneurApplication.class, args);
+	}
+
+}
